5fb17a40cdafa924609bf57d41ed61ecaf6c3677
Tom Wilent <tom.wilent@gmail.com>
2021-07-21 02:42:23 +0000

    Added more for the filtering, including data on hosts

--- nornir_script1.py
+++ nornir_script1.py
@@ -5,13 +5,16 @@ Gather Specific Facts about the Network
 from nornir import InitNornir
 from nornir_utils.plugins.functions import print_result
 from nornir_napalm.plugins.tasks import napalm_get
+from nornir.core.filter import F
 
-NR = InitNornir(config_file="config.yaml")
+nr = InitNornir(config_file="config.yaml")
+provider = nr.filter(F(groups__contains="area1") & ~F(groups__contains="area2"))
+customers = nr.filter(F(groups__contains="blue") & ~F(group__contains="red") & ~F(group__contains="green"))
 
-RESULTS = NR.run(
-        task=napalm_get, getters=["facts", "interfaces", "arp_table"]
+interfaces = customers.run(
+    task=napalm_get, getters="interfaces"
+    )
 
-)
+rtr_interfaces=print_result(interfaces)
 
-
-print_result(RESULTS)
+print (rtr_interfaces)
--- nornir_script_netmiko.py
+++ nornir_script_netmiko.py
@@ -7,13 +7,19 @@ from nornir_utils.plugins.functions import print_result
 from nornir_netmiko.tasks import netmiko_send_command, netmiko_send_config
 from nornir.core.filter import F
 
-NR = InitNornir(config_file="config.yaml")
+#provider = nr.filter(F(groups__contains="blue"))
+#provider = nr.filter(F(groups__contains="green"))
+#provider = nr.filter(F(groups__contains="red"))
+#provider = nr.filter(F(groups__contains="area2"))
+#provider = nr.filter(F(groups__contains="area2") & ~F(groups__contains="area1"))
 
-provider = NR.filter(F(has_parent_group="area1", "area2"))
+nr = InitNornir(config_file="config.yaml")
+provider = nr.filter(F(router_type="ce"))
+commands=["sho ip int br", "sho ip route"]
 
-RESULTS = provider.run(
-    task=netmiko_send_config, config_commands = ["do wr"]
-
-)
-
-print_result(RESULTS)
+for cmd in commands:
+    results = provider.run(
+        task=netmiko_send_command,
+        command_string=cmd
+        )
+    print_result(results)
