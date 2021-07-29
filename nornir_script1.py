352ceafa0fbac1df083d94afad17d93d95318fe5
Tom Wilent <tom.wilent@gmail.com>
2021-07-29 11:23:40 +0000

    making more changes

--- nornir_script1.py
+++ nornir_script1.py
@@ -11,10 +11,9 @@ nr = InitNornir(config_file="config.yaml")
 provider = nr.filter(F(groups__contains="area1") & ~F(groups__contains="area2"))
 customers = nr.filter(F(groups__contains="blue") & ~F(group__contains="red") & ~F(group__contains="green"))
 
-interfaces = customers.run(
+interfaces = provider.run(
     task=napalm_get, getters="interfaces"
     )
 
-rtr_interfaces=print_result(interfaces)
+print_result(interfaces)
 
-print (rtr_interfaces)
--- nornir_script_netmiko.py
+++ nornir_script_netmiko.py
@@ -7,15 +7,17 @@ from nornir_utils.plugins.functions import print_result
 from nornir_netmiko.tasks import netmiko_send_command, netmiko_send_config
 from nornir.core.filter import F
 
+nr = InitNornir(config_file="config.yaml")
 #provider = nr.filter(F(groups__contains="blue"))
 #provider = nr.filter(F(groups__contains="green"))
 #provider = nr.filter(F(groups__contains="red"))
 #provider = nr.filter(F(groups__contains="area2"))
-#provider = nr.filter(F(groups__contains="area2") & ~F(groups__contains="area1"))
+#provider = nr.filter(F(groups__contains="area2") | F(groups__contains="area1"))
+#provider_ce_only = provider.filter(router_type="pe")
+#ce_not_R1 = provider_ce_only.filter(~F(hostname="192.168.100.101"))
 
-nr = InitNornir(config_file="config.yaml")
 provider = nr.filter(F(router_type="ce"))
-commands=["sho ip int br", "sho ip route"]
+commands=["wr"]
 
 for cmd in commands:
     results = provider.run(
