60deec5e45bb826547a35a460ef238d4f32b6729
Tom Wilent <tom.wilent@gmail.com>
2021-07-31 04:26:31 +0000

    Added Junos version of scripts

--- nornir_junos_script1.py
+++ nornir_junos_script1.py
@@ -7,9 +7,8 @@ from nornir_utils.plugins.functions import print_result
 from nornir_napalm.plugins.tasks import napalm_get
 from nornir.core.filter import F
 
-nr = InitNornir(config_file="config.yaml")
-provider = nr.filter(F(groups__contains="area1") & ~F(groups__contains="area2"))
-customers = nr.filter(F(groups__contains="blue") & ~F(group__contains="red") & ~F(group__contains="green"))
+nr = InitNornir(config_file="config.junos.yaml")
+spine = nr.filter(F(groups__contains="evpn_spine"))
 
 interfaces = nr.run(
     task=napalm_get, getters="mac_address_table"
