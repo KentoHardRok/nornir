715bac62ad6bf1056be53ae61c15aa8297eb1ba4
Tom Wilent <tom.wilent@gmail.com>
2021-08-02 04:30:55 +0000

    Finally got the mac_tables in panads df

---
+++ nornir_get_mac.py
@@ -0,0 +1,29 @@
+"""
+Gather Specific Facts about the Network
+"""
+
+from nornir import InitNornir
+from nornir_napalm.plugins.tasks import napalm_get
+from nornir.core.filter import F
+import pandas as pd
+
+nr = InitNornir(config_file="config.junos.yaml")
+leaf = nr.filter(F(groups__contains="evpn_leaf"))
+
+mac_address = leaf.run(
+    task=napalm_get, getters="mac_address_table"
+    )
+
+dev_num = len(mac_address.keys())
+dev, value = zip(*mac_address.items())
+mac_df = []
+
+for i in range(0, dev_num):
+    print("*******************  " + dev[i] + "  ************************")
+    mac_df.append(pd.DataFrame(mac_address[dev[i]][0].result['mac_address_table']))
+    print(mac_df)
+    
+# total_mac_list = pd.concat(mac_df)
+# print(total_mac_list)
+# print(total_mac_list.drop_duplicates(subset=['mac']))
+
